38b313533cc19212d2c10b355fa805f979860267
aalekh42 <aalekhpradeep@gmail.com>
2022-03-13 13:15:37 +0530

    Created firebase confug file

---
+++ netflix/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBhjGYIoyMR00AgPtVbfdQwTsC7aWCmjJc",
+    authDomain: "netflix-clone-f417d.firebaseapp.com",
+    projectId: "netflix-clone-f417d",
+    storageBucket: "netflix-clone-f417d.appspot.com",
+    messagingSenderId: "175180599070",
+    appId: "1:175180599070:web:adf77c274bf388be9ec30f",
+    measurementId: "G-6823Z1EGDB"
+  };
