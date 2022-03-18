ec1aa21fe66f741acb4c4defb8987ca338e0860c
aalekh42 <aalekhpradeep@gmail.com>
2022-03-18 15:02:27 +0530

    Few more css changes

--- netflix/src/components/Banner.js
+++ netflix/src/components/Banner.js
@@ -33,7 +33,7 @@ function Banner() {
       style={{
         backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundSize: "cover",
-        backgroundPosition: "center center",
+        // backgroundPosition: "center center",
       }}
     >
       <div className="banner_contents">
